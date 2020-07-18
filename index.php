554656aa47676939b638b0e7ab244ceb850c36f8
RobertWatkin <robert_watkin@yahoo.co.uk>
2020-07-18 15:44:44 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!--  Created By Robert Watkin - RobertWatkin, GitHub
+__________      ___.                  __     __      __         __   __   .__
+\______   \ ____\_ |__   ____________/  |_  /  \    /  \_____ _/  |_|  | _|__| ____
+ |       _//  _ \| __ \_/ __ \_  __ \   __\ \   \/\/   /\__  \\   __\  |/ /  |/    \
+ |    |   (  <_> ) \_\ \  ___/|  | \/|  |    \        /  / __ \|  | |    <|  |   |  \
+ |____|_  /\____/|___  /\___  >__|   |__|     \__/\  /  (____  /__| |__|_ \__|___|  /
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include("php/head.php"); ?>
+</head>
+<body>
+
+  <!--Navbar-->
+  <?php include("php/navbar.php"); ?>
+
+  <!--Main-->
+  <!-- Header - set the background image for the header in the line below -->
+  <header class="py-5 bg-image-full" style="background-image: url('images/lotto-balls.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center;">
+    <img class="img-fluid d-block mx-auto" style="height: 150px;" src="" alt="">
+  </header>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <h1>About Us</h1>
+      <p class="lead">A little more personal...</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </section>
+
+  <!-- Image element - set the background image for the header in the line below -->
+  <div class="py-5 bg-image-full" style="background-image: url('images/red_car.jpg');  background-repeat: no-repeat; background-size: cover; background-position: center;"">
+    <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+    <div style="height: 200px;"></div>
+  </div>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <h1>Our Services</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+    </div>
+  </section>
+
+
+  <!--Footer-->
+  <?php include("php/footer.php"); ?>
+</body>
+</html>
---
+++ php/footer.php
@@ -0,0 +1,38 @@
+  <!-- Site footer -->
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Enjoy many thrilling competitions here at the Big Prize Draw! We are a UK based competition company aiming to give you exciting prizes and competitions at amazing prices.</p>
+          </div>
+
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.php">About</a></li>
+              <li><a href="contact.php">Contact Us</a></li>
+              <li><a href="#">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+         <a href="#">the Big Prize Draw</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com/groups/682779282509700"><i class="fa fa-facebook"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer> 
